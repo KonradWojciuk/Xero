e3a27503ec638326f50e3f39c695fab994bb4cd5
konradwojciuk <konrad.wojciuk@microsoft.wsei.edu.pl>
2023-05-17 11:51:05 +0200

    Dodaj pliki projektów.

---
+++ Zadanie1/Copier.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using CopierLibrary;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public new int Counter { get; private set; } = 0;
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        public Copier() { }
+        public new void PowerOn()
+        {
+            if (state == IDevice.State.on)
+                return;
+            base.PowerOn();
+            Counter++;
+        }
+        public new void PowerOff()
+        {
+            if (state == IDevice.State.off)
+                return;
+            base.PowerOff();
+        }
+        public void Print(in IDocument document)
+        {
+            if (GetState() == IDevice.State.off)
+                return;
+
+            string format = document.GetFormatType().ToString().ToLower();
+
+            Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}.{format}");
+            PrintCounter++;
+        }
+        public void Scan(out IDocument document, IDocument.FormatType formatType = default)
+        {
+            IDocument? scanDocument;
+
+            if (GetState() == IDevice.State.off)
+            {
+                document = default;
+                return;
+            }
+
+            switch (formatType)
+            {
+                case IDocument.FormatType.TXT:
+                    scanDocument = new TextDocument($"TextScan{ScanCounter}");
+                    break;
+                case IDocument.FormatType.PDF:
+                    scanDocument = new PDFDocument($"PDFScan{ScanCounter}");
+                    break;
+                case IDocument.FormatType.JPG:
+                    scanDocument = new ImageDocument($"ImageScan{ScanCounter}");
+                    break;
+                default:
+                    scanDocument = default;
+                    break;
+
+            }
+
+            string format = scanDocument.GetFormatType().ToString().ToLower();
+
+            Console.WriteLine($"{DateTime.Now} Scan: {scanDocument.GetFileName()}.{format}");
+            document = scanDocument;
+            ScanCounter++;
+        }
+        public void ScanAndPrint()
+        {
+            if (GetState() == IDevice.State.off)
+                return;
+
+            IDocument document = new ImageDocument($"ImageScan{ScanCounter}");
+            string format = document.GetFormatType().ToString().ToLower();
+
+            Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}.{format}");
+            Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+
+            ScanCounter++;
+            PrintCounter++;
+        }
+    }
+}
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,61 @@
+﻿using CopierLibrary;
+
+namespace Zadanie1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Copier copier = new Copier();
+            copier.PowerOn();
+            copier.PowerOn();
+            copier.PowerOn();
+
+            IDocument doc1 = new PDFDocument("test");
+            IDocument doc2 = new TextDocument("test");
+            IDocument doc3 = new ImageDocument("test");
+
+
+            copier.Print(doc1);
+            copier.Print(doc2);
+            copier.Print(doc3);
+            copier.Print(doc1);
+
+            copier.PowerOff();
+            copier.Print(doc1);
+            copier.PowerOff();
+            copier.PowerOff();
+            copier.Print(doc2);
+
+            copier.PowerOn();
+            copier.Scan(out doc1, IDocument.FormatType.PDF);
+            copier.Scan(out doc2, IDocument.FormatType.TXT);
+            copier.Scan(out doc3, IDocument.FormatType.JPG);
+
+            copier.PowerOff();
+            copier.Scan(out doc1, IDocument.FormatType.PDF);
+            copier.Scan(out doc2, IDocument.FormatType.TXT);
+            copier.PowerOff();
+            copier.Scan(out doc3, IDocument.FormatType.JPG);
+            copier.PowerOn();
+            copier.Scan(out doc3, IDocument.FormatType.JPG);
+
+            copier.ScanAndPrint();
+            copier.ScanAndPrint();
+            copier.ScanAndPrint();
+            copier.ScanAndPrint();
+
+            copier.PowerOff();
+            copier.ScanAndPrint();
+            copier.ScanAndPrint();
+            copier.PowerOff();
+            copier.ScanAndPrint();
+            copier.PowerOn();
+            copier.ScanAndPrint();
+
+            Console.WriteLine("Liczba włęczeń drukarki: " + copier.Counter);
+            Console.WriteLine("Liczba skanów: " + copier.ScanCounter);
+            Console.WriteLine("Liczba druków: " + copier.PrintCounter);
+        }
+    }
+}
